328cafa226ca7fcace9f8a545e4158c78c990e6d
owenschiller <owenschiller14@gmail.com>
2024-09-11 13:50:57 -0700

    added carousel to soccer page

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
 	$(".first").click(function(){
 		$(this).next().slideToggle("slow");
 	});
-
+	$(".caro").click(function(){
+		$(this).nextAll().filter(".hiddenCaro").slideToggle("slow");
+	});
 	
 });
